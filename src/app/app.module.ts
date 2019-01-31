7df679d79e0d7708475a431ef890987c6cd2616f
Daniel Sobota <daniel.sobota@codemag.pl>
2019-01-31 22:51:13 +0100

    New contacts list layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule, MatExpansionModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatInputModule, MatCardModule } from '@angular/material';
 import { ContactsComponent } from './contacts/contacts.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -27,8 +27,8 @@ import { FormsModule } from '@angular/forms';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatExpansionModule,
     MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact.ts
+++ src/app/contact.ts
@@ -1,10 +1,12 @@
 export class Contact {
     id: number;
+    avatar: string;
     name: string;
     number: string;
 
-    constructor(id, name, number) {
+    constructor(id, avatar, name, number) {
         this.id = id;
+        this.avatar = avatar;
         this.name = name;
         this.number = number;
     }
