1aa675e0f5731bbd130e68f3cec90eab0afc3e29
Daniel Sobota <daniel.sobota@codemag.pl>
2019-01-31 01:43:26 +0100

    App navigation

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+
+    @Input() appTitle: string;
+
+    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+            map(result => result.matches)
+        );
+
+    constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
